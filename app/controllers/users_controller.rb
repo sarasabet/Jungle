2b25c9efafa7c656bdb521c8d0cdbd69babf8ef3
Sara Sabet <sara.ilbeigi@gmail.com>
2021-10-14 23:44:48 +0000

    add signup vew and creat method to user control

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+  private
+
+# add the private user_params method to sanitize the input from the form (this is a new Rails 4 thing and it's required). 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
+end
