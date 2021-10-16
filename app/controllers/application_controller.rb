b28b6e6b757742765aeab961246c55c6b96843eb
Sara Sabet <sara.ilbeigi@gmail.com>
2021-10-16 03:20:47 +0000

    fix confilitcs on merging feature/user-auth  to master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,10 +31,6 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
-<<<<<<< HEAD
-=======
-
->>>>>>> feature/user-auth
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
