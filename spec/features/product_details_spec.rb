e9437cd3cc551e7d447c69d0366d47989bb4b13a
Sara Sabet <sara.ilbeigi@gmail.com>
2021-10-19 22:28:29 +0000

    add test to product_details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+
+    end
+  end
+
+  scenario "They see the product detail page" do
+    # ACT
+    visit root_path
+    # to get the first child of article 
+    first('article.product img').click
+
+    #DEBUG
+    sleep 5
+    save_screenshot
+    
+    # VERIFY
+    #  havetext/have_content are same 
+    expect(page).to have_text("#{@category.name} » #{Product.last.name}")
+    expect(page).to have_content("#{@category.name} » #{Product.last.name}")
+   
+  end
+end
