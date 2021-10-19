0313d1f88fa32e7c7c7922d60c9cbdd60cba1a20
Sara Sabet <sara.ilbeigi@gmail.com>
2021-10-19 13:29:37 +0000

    add user-spec models

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create
+
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
