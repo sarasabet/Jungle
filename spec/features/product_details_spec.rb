9a5f1b1398dad6aabdbd717c149a869f91dfb61e
Sara Sabet <sara.ilbeigi@gmail.com>
2021-10-20 13:38:35 +0000

    fix login/signin presentation on nav bar

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,39 +1,39 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
-#   # SETUP
-#   before :each do
-#     @category = Category.create! name: 'Apparel'
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
 
-#     10.times do |n|
-#       @category.products.create!(
-#         name:  Faker::Hipster.sentence(3),
-#         description: Faker::Hipster.paragraph(4),
-#         image: open_asset('apparel1.jpg'),
-#         quantity: 10,
-#         price: 64.99
-#       )
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
 
-#     end
-#   end
+    end
+  end
 
-#   scenario "They see the product detail page" do
-#     # ACT
-#     visit root_path
-#     # to get the first child of article 
-#     # first('article.product img').click
-#     all('article.product img').first.click
+  scenario "They see the product detail page" do
+    # ACT
+    visit root_path
+    # to get the first child of article 
+    # first('article.product img').click
+    all('article.product img').first.click
 
-#     #DEBUG
-#     sleep 5
-#     save_screenshot
+    #DEBUG
+    sleep 5
+    save_screenshot
     
-#     # VERIFY
-#     #  havetext/have_content are same 
-#     expect(page).to have_text("#{@category.name} » #{Product.last.name}")
-#     expect(page).to have_content("#{@category.name} » #{Product.last.name}")
+    # VERIFY
+    #  havetext/have_content are same 
+    expect(page).to have_text("#{@category.name} » #{Product.last.name}")
+    expect(page).to have_content("#{@category.name} » #{Product.last.name}")
  
    
-#   end
-# end
+  end
+end
